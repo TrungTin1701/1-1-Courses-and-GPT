443c523948a055ea0764c07c345fd65a302997cd
TrungTin1701 <63804279+TrungTin1701@users.noreply.github.com>
2023-03-29 23:03:06 +0700

    UpdateUI

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:oneonecourse_app/Moodel/Courses.dart';
-import 'package:oneonecourse_app/screens/DetailsCourses/Course_Booked.dart';
 import 'package:oneonecourse_app/screens/DetailsCourses/Courses_Screen.dart';
-import 'package:oneonecourse_app/screens/DetailsCourses/HistoryCourse.dart';
-import 'package:oneonecourse_app/screens/Teachers/detailsTeacher.dart';
-import 'package:oneonecourse_app/screens/VideoCall.dart';
 import 'package:oneonecourse_app/screens/chart.dart';
-import 'package:oneonecourse_app/screens/home.dart';
 import 'package:oneonecourse_app/screens/root_app.dart';
-import 'Moodel/Teacher.dart';
-import 'screens/DetailsCourses/Detail_Course.dart';
 import 'screens/LoginPage/login_Page.dart';
 import 'theme/color.dart';
+import 'package:device_preview/device_preview.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -24,58 +25,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Online Course App',
-      routes: {
-        '/login': (context) => LoginPage(),
-        '/home': (context) => RootApp(),
-        '/chart': (context) => ChatPage(),
-        '/detailsteacher': (context) => ChatPage(),
-        'detailscourses': (context) => CoursesScreen(courses: Courses(
-        name: "Flutter",
-        image: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png",
-        description: "Flutter is Google’s UI toolkit for building beautiful, natively compiled applications for mobile, web, and desktop from a single codebase. Flutter works with existing code, is used by developers and organizations around the world, and is free and open source.",
-        teacher: "Nguyen Van A",
-        category: "Mobile",
-        price: "Free",
-        duration: "2h",
-        review: "100",
-        session: "10",
-        rating: "4.5",
-        dateTime: DateTime.now(),
-
-        ) ),
-        
-      },
-      theme: ThemeData(
-        primaryColor: primary,
-      ),
-      home:HistoryCourse(teacher: Teachers(
-      rate: 4.5,
-          name: 'Nguyen Van A',
-          image: 'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png',
-          email: 'tipha230@gmail.com',
-          phone: '0123456789',
-          intro: 'Hello, I am a teacher',
-          country: "France",
-
-      
-      ),
-      course: Courses(
-            name: "Flutter",
-        image: "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png",
-        description: "Flutter is Google’s UI toolkit for building beautiful, natively compiled applications for mobile, web, and desktop from a single codebase. Flutter works with existing code, is used by developers and organizations around the world, and is free and open source.",
-        teacher: "Nguyen Van A",
-        category: "Mobile",
-        price: "Free",
-        duration: "2h",
-        review: "100",
-        session: "10",
-        rating: "4.5",
-        dateTime: DateTime.now(),
-      ),
-      ),
-    );
+        builder: DevicePreview.appBuilder,
+        locale: DevicePreview.locale(context),
+        debugShowCheckedModeBanner: false,
+        title: 'Online Course App',
+        routes: {
+          '/login': (context) => LoginPage(),
+          '/home': (context) => RootApp(),
+          '/chart': (context) => ChatPage(),
+          '/detailsteacher': (context) => ChatPage(),
+          'detailscourses': (context) => CoursesScreen(
+                  courses: Courses(
+                name: "Flutter",
+                image:
+                    "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png",
+                description:
+                    "Flutter is Google’s UI toolkit for building beautiful, natively compiled applications for mobile, web, and desktop from a single codebase. Flutter works with existing code, is used by developers and organizations around the world, and is free and open source.",
+                teacher: "Nguyen Van A",
+                category: "Mobile",
+                price: "Free",
+                duration: "2h",
+                review: "100",
+                session: "10",
+                rating: "4.5",
+                dateTime: DateTime.now(),
+              )),
+        },
+        theme: ThemeData(
+          primaryColor: primary,
+        ),
+        home: RootApp());
   }
-
 }
--- lib/screens/Teachers/list_teachers.dart
+++ lib/screens/Teachers/list_teachers.dart
@@ -5,75 +5,85 @@ import 'package:flutter/material.dart';
 import 'package:oneonecourse_app/Moodel/Teacher.dart';
 import 'package:oneonecourse_app/screens/Teachers/detailsTeacher.dart';
 import 'package:oneonecourse_app/widgets/card_teacher.dart';
+
 class ListTeach extends StatelessWidget {
   const ListTeach({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       child: CarouselSlider(
-        options: CarouselOptions(
-          enlargeCenterPage: true,
-          autoPlay: true,
-          aspectRatio: 16 / 9,
-          autoPlayCurve: Curves.fastOutSlowIn,
-          enableInfiniteScroll: true,
-          autoPlayAnimationDuration: Duration(milliseconds: 800),
-          viewportFraction: 0.8,
-        ),
-        items: [
-          CardTeacher(teacher: Teachers(
-          rate: 4.5,
-          name: 'Nguyen Van A',
-          image: 'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png',
-          email: 'tipha230@gmail.com',
-          phone: '0123456789',
-          intro: 'Hello, I am a teacher',
-          country: "France",
-          
-        ),
-        onTap: (){
-          print("object");
-         Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsTeacher(teacher: Teachers(
-          rate: 4.5,
-          name: 'Nguyen Van A',
-          image: 'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png',
-          email: 'tipha230@gmail.com',
-          phone: '0123456789',
-          intro: 'Hello, I am a teacher',
-          country: "France",
-          
-        ),)));
-        },
-           ),
-           CardTeacher(teacher: Teachers(
-          rate: 4.5,
-          name: 'Nguyen Van A',
-          image: 'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png',
-          email: 'tipha230@gmail.com',
-          phone: '0123456789',
-          intro: 'Hello, I am a teacher',
-          country: "France"
-          
-        ),
-        onTap: (){},
-           ),
-           CardTeacher(teacher: Teachers(
-          rate: 4.5,
-          name: 'Nguyen Van A',
-          image: 'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png',
-          email: 'tipha230@gmail.com',
-          phone: '0123456789',
-          intro: 'Hello, I am a teacher',
-          country: "France"
-
-          
-        ),
-        onTap: (){},
-           ),
-        ]
-      ),
-
-      ); 
+          options: CarouselOptions(
+            enlargeCenterPage: true,
+            autoPlay: true,
+            aspectRatio: 16 / 9,
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enableInfiniteScroll: true,
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            viewportFraction: 0.8,
+          ),
+          items: [
+            CardTeacher(
+              teacher: Teachers(
+                  rate: 4.5,
+                  name: 'Nguyen Van A',
+                  image:
+                      'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png',
+                  email: 'tipha230@gmail.com',
+                  phone: '0123456789',
+                  intro: 'Hello, I am a teacher',
+                  country: "France",
+                  type: {Types.Business, Types.Education, Types.Kids}),
+              onTap: () {
+                print("object");
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailsTeacher(
+                              teacher: Teachers(
+                                  rate: 4.5,
+                                  name: 'Nguyen Van A',
+                                  image:
+                                      'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png',
+                                  email: 'tipha230@gmail.com',
+                                  phone: '0123456789',
+                                  intro: 'Hello, I am a teacher',
+                                  country: "France",
+                                  type: {
+                                    Types.Business,
+                                    Types.Education,
+                                    Types.Kids
+                                  }),
+                            )));
+              },
+            ),
+            CardTeacher(
+              teacher: Teachers(
+                  rate: 4.5,
+                  name: 'Nguyen Van A',
+                  image:
+                      'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png',
+                  email: 'tipha230@gmail.com',
+                  phone: '0123456789',
+                  intro: 'Hello, I am a teacher',
+                  country: "France",
+                  type: {Types.Business, Types.Education, Types.Kids}),
+              onTap: () {},
+            ),
+            CardTeacher(
+              teacher: Teachers(
+                  rate: 4.5,
+                  name: 'Nguyen Van A',
+                  image:
+                      'https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651_960_720.png',
+                  email: 'tipha230@gmail.com',
+                  phone: '0123456789',
+                  intro: 'Hello, I am a teacher',
+                  country: "France",
+                  type: {Types.Business, Types.Education, Types.Kids}),
+              onTap: () {},
+            ),
+          ]),
+    );
   }
 }
